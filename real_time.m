cb754cc07ab3bf793f5f905c1f15399b9dc927d5
RomanKoshkin <roman.koshkin@gmail.com>
2018-02-09 03:13:53 +0400

    some more

--- real_time.m
+++ real_time.m
@@ -1,13 +1,18 @@
 load('EEG.mat')
 load('output.mat')
-counter = 0;
+%%
+
+    ch_left = find(ismember({EEG.chanlocs.labels}, 'Left_AUX') == 1);
+    ch_right = find(ismember({EEG.chanlocs.labels},'Right_AUX') == 1);
+    Fs = EEG.srate;
+
+compute_envelope = 1;
 Fs = EEG.srate
 or = 0;    % kernel origin, ms
 en = 500;
-step_s = 1;
-
-for start = round(1:Fs*step_s:length(EEG.data)-Fs*30)
-    
+steps = round(1:Fs*1:length(EEG.data)-Fs*30);
+parfor step_no = 1:length(steps)
+    start = steps(step_no)
     fin = start + Fs*30;
 
     if compute_envelope == 1
@@ -20,18 +25,19 @@ for start = round(1:Fs*step_s:length(EEG.data)-Fs*30)
 
     response = EEG.data(1:60, start:fin)';
 
-    counter = counter + 1;
+    [recon, a_r_left(step_no), p, a_MSE_left(step_no)] = mTRFpredict(stimLeft, response, g_att, Fs, 1, or, en, Lcon);
+    [recon, a_r_right(step_no), p, S(step_no).a_MSE_right(step_no)] = mTRFpredict(stimRight, response, g_att, Fs, 1, or, en, Lcon);
 
-    [recon, a_r_left(counter), p, a_MSE_left(counter)] = mTRFpredict(stimLeft, response, g_att, Fs, 1, or, en, Lcon);
-    [recon, a_r_right(counter), p, S(counter).a_MSE_right(counter)] = mTRFpredict(stimRight, response, g_att, Fs, 1, or, en, Lcon);
-
-    [recon, u_r_left(counter), p, u_MSE_left(counter)] = mTRFpredict(stimLeft, response, g_unatt, Fs, 1, or, en, Lcon);
-    [recon, u_r_right(counter), p, u_MSE_right(counter)] = mTRFpredict(stimRight, response, g_unatt, Fs, 1, or, en, Lcon);
+    [recon, u_r_left(step_no), p, u_MSE_left(step_no)] = mTRFpredict(stimLeft, response, g_unatt, Fs, 1, or, en, Lcon);
+    [recon, u_r_right(step_no), p, u_MSE_right(step_no)] = mTRFpredict(stimRight, response, g_unatt, Fs, 1, or, en, Lcon);
     
-    disp(counter)
+    disp(step_no)
 end
+save('output2.mat', 'a_r_left', 'u_r_left', 'u_r_right', 'a_r_right')
 
-% computed correleations within sliding windows using the ATTENDED decoder:
+%% computed correleations within sliding windows using the ATTENDED decoder:
+load('output2.mat') 
+
 figure; 
 subplot(2,1,1)
 Fs = EEG.srate;
@@ -39,7 +45,9 @@ plot(1:1269,a_r_left,1:1269, a_r_right, 'LineWidth', 0.5)
 times = (round(1:Fs*1:length(EEG.data)-Fs*30)-1)/Fs;
 ax = gca;
 ax.XTick = linspace(0,length(times),10);
-
+title('attended decoders')
+ylabel('Correlation')
+xlabel('Time, s')
 for i = 5:4:61
     	on = EEG.event(i).latency/EEG.srate;
         off = EEG.event(i+1).latency/EEG.srate;
@@ -68,6 +76,9 @@ plot(1:1269,u_r_left,1:1269, u_r_right, 'LineWidth', 0.5)
 times = (round(1:Fs*1:length(EEG.data)-Fs*30)-1)/Fs;
 ax = gca;
 ax.XTick = linspace(0,length(times),10);
+title('UNattended decoders')
+ylabel('Correlation')
+xlabel('Time, s')
 
 for i = 5:4:61
     	on = EEG.event(i).latency/EEG.srate;
